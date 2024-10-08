4793ae05de7807823d563ffb0c13d30b7c7c9b07
Arnoldfon <arnold.fon.mail@gmail.com>
2024-10-08 16:45:41 +0300

    Initial creation with  only one user "arnold"

---
+++ security.py
@@ -0,0 +1,11 @@
+name = input("What is your Name: ")
+
+print("Please enter your name: \n")
+
+if name == "Arnold":
+  print("Your are Welcome sir \n")
+  print("Door Open!!!!\n")
+
+else:
+  print("Access Denied!!!")
+
