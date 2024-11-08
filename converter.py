f8689e335bbe80e343614ee3d8bd35c5359eb44b
Arnoldfon <arnold.fon.mail@gmail.com>
2024-11-08 16:36:52 +0300

    innitial file creation

---
+++ converter.py
@@ -0,0 +1,11 @@
+# This is a converter application
+
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = #input the miles here
+kilometers_to_miles = #input the kilometer here
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+
